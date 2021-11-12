7d21b4e3285a0670bb7e4868a13261311463f0fd
ziyadloubaris <ziyadloubaris@gmail.com>
2021-11-12 23:52:43 +0000

    Création d'un bean géré par CDI

---
+++ src/main/java/managedbeans/CustomerMBean.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package managedbeans;
+
+import com.mycompany.tp1loubarisziyad.entities.Customer;
+import com.mycompany.tp1loubarisziyad.session.CustomerManager;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import java.util.List; 
+/**
+ *
+ * @author ziyad
+ */
+@Named(value = "customerMBean")
+@ViewScoped
+public class CustomerMBean implements Serializable {
+    
+    private List<Customer> customerList;
+    @EJB
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerMBean
+     */
+    public CustomerMBean() {
+    }
+    
+    public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
