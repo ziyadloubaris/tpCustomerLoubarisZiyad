c31e757ac7b55052f1767e569e14c996d1b54673
ziyadloubaris <ziyadloubaris@gmail.com>
2021-11-12 22:12:54 +0000

    Configuration pour JSF

---
+++ src/main/java/com/mycompany/tp1loubarisziyad/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp1loubarisziyad.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+/**
+ *
+ * @author ziyad
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
